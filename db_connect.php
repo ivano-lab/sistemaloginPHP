03a661f3f7014ff622b3472f9abe2b9f51a4c0ef
Ívano <75231556+ivano-lab@users.noreply.github.com>
2024-05-17 23:45:42 -0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 
 $servername="localhost";
 $username = "root";
-$password = "ivano!@#";
+$password = "*******";
 $db_name = "sistemalogin";
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
